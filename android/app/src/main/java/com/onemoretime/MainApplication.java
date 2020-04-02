86bc34c35c0d8bbb212e29565774607510fa7e67
NgocNam3518 <ngocnam3518@gmail.com>
2020-04-02 11:04:35 +0700

    use notification successful

--- android/app/src/main/java/com/onemoretime/MainApplication.java
+++ android/app/src/main/java/com/onemoretime/MainApplication.java
@@ -12,6 +12,7 @@ import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import io.invertase.firebase.messaging.RNFirebaseMessagingPackage; // <-- Add this line
+import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -29,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
           packages.add(new RNFirebaseMessagingPackage()); // <-- Add this line
+          packages.add(new RNFirebaseNotificationsPackage());
           return packages;
         }
 
