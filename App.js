ac94b2093c1f493cbb24813e409cd8e3e6ee42f4
NgocNam3518 <ngocnam3518@gmail.com>
2020-04-02 11:36:49 +0700

    receive message successfully

--- App.js
+++ App.js
@@ -1,16 +1,59 @@
-import React, { Component } from "react";
-import {View, StyleSheet, Text} from "react-native";
+import React, {Component} from 'react'
+import {View, StyleSheet, Text} from 'react-native'
+import {fcmService} from './src/FCMService'
 
 export default class App extends Component {
   constructor(props) {
     super(props)
   }
 
-  render() {
-    let {container} = styles;
+  componentDidMount() {
+    fcmService.register(this.onRegister, this.onNotification, 
+    this.onOpenNotification)
+  }
+
+  onRegister(token) {
+    console.log("[NotificationFCM] onRegister: ", token)
+  }
+
+  onNotification(notify) {
+    console.log("[NotificationFCM] onNotification: ", notify)
+    // For Android
+    const channelObj = {
+      channelId: "SampleChannelID",
+      channelName: "SampleChannelName",
+      channelDes: "SampleChannelDes"
+    }
+    const channel = fcmService.buildChannel(channelObj)
+
+    const buildNotify = {
+      dataId: notify._notificationId,
+      title: notify._title,
+      content: notify._body,
+      sound: 'default',
+      channel: channel,
+      data: {},
+      colorBgIcon: "#1A243B",
+      largeIcon: 'ic_launcher',
+      smallIcon: 'ic_launcher',
+      vibrate: true
+    }
+
+    const notification = fcmService.buildNotification(buildNotify)
+    fcmService.displayNotification(notification)
+
+  }
+
+  onOpenNotification(notify) {
+    console.log("[NotificationFCM] onOpenNotification: ", notify)
+    alert("Open Notification: " + notify._body)
+  }
+
+  render () {
+    let {container} = styles
     return (
       <View style={container}>
-        <Text>Sample Text</Text>
+        <Text>Sample React Native Firebase</Text>
       </View>
     )
   }
